4467d9472376b57015753080848c3b6bae46e074
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-02-27 03:57:51 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,8 +4,32 @@ public class PracticeProblem {
 
 	}
 
-	public static int min(int num1, int num2, int num3) {
-		return num1;
+	public static String pluralize(String word) {
+		word = word.toLowerCase();
+		if (word.endsWith("ey")) {
+			return "eys";
+		} else if (word.endsWith("y")) {
+			return "ies";
+		} else if (word.endsWith("ife")) {
+			return "ives";
+		}
+		return "s";
 	}
 
+	public static int min(int num, int num2, int num3) {
+		int smallest = num;
+		if (num2 <= smallest) {
+			smallest = num2;
+		} if (num3 <= smallest) {
+			smallest = num3;
+		}
+		return smallest;
+	}
+
+	public static boolean isLeapYear(int num) {
+		if ((num % 4 == 0) && (num % 100 != 0) || (num % 400 == 0)) {
+			return true;
+		}
+		return false;
+	}
 }
